8f612bc16074e2a7c756bce7cc8fd7f77ab894b5
ENaveen98 <enaveen98@gmail.com>
2020-04-23 10:25:23 +0530

    Updates

---
+++ precache-manifest.34059854fd68f081e18b05f7fd25d476.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5948c88dc12c4952f361ec55e29b3884",
+    "url": "/rqn/index.html"
+  },
+  {
+    "revision": "ae4d755a2203e71ea5a1",
+    "url": "/rqn/static/css/main.cae8f572.chunk.css"
+  },
+  {
+    "revision": "57ca4020ab7a8defc5a5",
+    "url": "/rqn/static/js/2.be037116.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rqn/static/js/2.be037116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4d755a2203e71ea5a1",
+    "url": "/rqn/static/js/main.64a23bf0.chunk.js"
+  },
+  {
+    "revision": "e5869e55464035a36890",
+    "url": "/rqn/static/js/runtime-main.c99c59df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rqn/static/media/logo.5d5d9eef.svg"
+  }
+]);
